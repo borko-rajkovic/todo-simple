75a23d2f6f2d57d284a54659c3d49e498e50d821
Borko <rajkovicborko@gmail.com>
2019-04-26 06:52:37 +0200

    Configure linter and run lint script

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -18,6 +18,7 @@ const isLocalhost = Boolean(
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
 );
 
+// tslint:disable-next-line: interface-over-type-literal
 type Config = {
   onSuccess?: (registration: ServiceWorkerRegistration) => void;
   onUpdate?: (registration: ServiceWorkerRegistration) => void;
