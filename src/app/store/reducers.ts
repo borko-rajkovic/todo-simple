76c8bcc96f52e2eb023c1859481be648ff636fe0
Borko Rajkovic <rajkovicborko@gmail.com>
2019-04-28 00:15:59 +0200

    createTodo - add, compose, max, pluck, prepend, reduce

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,6 +1,6 @@
 import { connectRouter, RouterState } from 'connected-react-router';
 import { History } from 'history';
-import { assoc, curry, map, propEq, reject, when } from 'ramda';
+import { add, assoc, compose, map, max, pluck, prepend, propEq, reduce, reject, when } from 'ramda';
 import { combineReducers } from 'redux';
 
 import { Todo } from '../models/Todo';
@@ -13,8 +13,32 @@ export interface AppState {
   todos: Todo[];
 }
 
+// these functions are called in order
+const pluckIds = (state: Todo[]) => pluck('id', state);
+const getMaxId = (ids: number[]) => reduce(max, -1, ids);
+const addOne = (reducedValue: number) => add(reducedValue, 1);
+
+// extract ids from todo list; get max id; increment by one
+const getNewId = compose(
+  addOne,
+  getMaxId,
+  pluckIds
+);
+
+const createTodo = (state: Todo[], text: string, timestamp: Date) =>
+  prepend(
+    {
+      id: getNewId(state),
+      completed: false,
+      text,
+      timestamp
+    },
+    state
+  );
+
 // change property 'completed' to given value for given id in todos
-const markTodo = curry((value, id, todos) => map(when(propEq('id', id), assoc('completed', value)), todos));
+const markTodo = (value: boolean, id: number, todos: Todo[]) =>
+  map(when(propEq('id', id), assoc('completed', value)), todos);
 
 // return todos which don't have given id
 const deleteTodo = (id: number, state: Todo[]) => reject(propEq('id', id), state);
@@ -22,15 +46,9 @@ const deleteTodo = (id: number, state: Todo[]) => reject(propEq('id', id), state
 export function todoReducer(state = initialState, action: TodoActionTypes): Todo[] {
   switch (action.type) {
     case CREATE_TODO:
-      return [
-        {
-          id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-          completed: false,
-          text: action.payload.text,
-          timestamp: new Date()
-        },
-        ...state
-      ];
+      const { text } = action.payload;
+      const timestamp = new Date();
+      return createTodo(state, text, timestamp);
     case MARK_TODO:
       const { completed, id } = action.payload;
       return markTodo(completed, id, state);
