651a3651304af8c4582338213db0ebcd495bfde5
Borko Rajkovic <rajkovicborko@gmail.com>
2019-04-27 15:06:43 +0200

    Fix rendering of App without crashing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 
+import { ConnectedRouter } from 'connected-react-router';
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import { NavLink, Redirect, Route, Router, Switch } from 'react-router-dom';
 
 import ListTodos from './app/pages/ListTodos';
 import MakeNewTodo from './app/pages/MakeNewTodo';
-import { history } from './app/store/configure-store';
+import configureStore, { history } from './app/store/configure-store';
 import logo from './logo.svg';
+
+const store = configureStore();
 
 export enum Routes {
   all = '/all',
@@ -18,51 +22,55 @@ export enum Routes {
 class App extends Component {
   public render() {
     return (
-      <Router history={history}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <div className="menu">
-            <ul>
-              <li>
-                {' '}
-                <NavLink to={Routes.all} activeStyle={{ color: 'red' }} exact={true}>
-                  All
-                </NavLink>{' '}
-              </li>
-              <li>
-                {' '}
-                <NavLink to={Routes.uncompleted} activeStyle={{ color: 'red' }} exact={true}>
-                  Uncompleted
-                </NavLink>{' '}
-              </li>
-              <li>
-                {' '}
-                <NavLink to={Routes.completed} activeStyle={{ color: 'red' }} exact={true}>
-                  Completed
-                </NavLink>{' '}
-              </li>
-              <li>
-                {' '}
-                <NavLink to={Routes.makeNew} activeStyle={{ color: 'red' }} exact={true}>
-                  Make new
-                </NavLink>{' '}
-              </li>
-            </ul>
-          </div>
-          <div className="App-content">
-            <Switch>
-              <Route path={Routes.all} component={ListTodos} />
-              <Route path={Routes.completed} component={ListTodos} />
-              <Route path={Routes.uncompleted} component={ListTodos} />
-              <Route path={Routes.makeNew} component={MakeNewTodo} />
-              <Redirect to={Routes.all} />
-            </Switch>
-          </div>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Router history={history}>
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              <div className="menu">
+                <ul>
+                  <li>
+                    {' '}
+                    <NavLink to={Routes.all} activeStyle={{ color: 'red' }} exact={true}>
+                      All
+                    </NavLink>{' '}
+                  </li>
+                  <li>
+                    {' '}
+                    <NavLink to={Routes.uncompleted} activeStyle={{ color: 'red' }} exact={true}>
+                      Uncompleted
+                    </NavLink>{' '}
+                  </li>
+                  <li>
+                    {' '}
+                    <NavLink to={Routes.completed} activeStyle={{ color: 'red' }} exact={true}>
+                      Completed
+                    </NavLink>{' '}
+                  </li>
+                  <li>
+                    {' '}
+                    <NavLink to={Routes.makeNew} activeStyle={{ color: 'red' }} exact={true}>
+                      Make new
+                    </NavLink>{' '}
+                  </li>
+                </ul>
+              </div>
+              <div className="App-content">
+                <Switch>
+                  <Route path={Routes.all} component={ListTodos} />
+                  <Route path={Routes.completed} component={ListTodos} />
+                  <Route path={Routes.uncompleted} component={ListTodos} />
+                  <Route path={Routes.makeNew} component={MakeNewTodo} />
+                  <Redirect to={Routes.all} />
+                </Switch>
+              </div>
+            </div>
+          </Router>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
